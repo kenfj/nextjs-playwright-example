21fa22ac25863abe633148d9222c0ce929f1b8aa
Ken <44661517+kenfj@users.noreply.github.com>
2023-12-02 17:19:25 +0900

    add hello page and update test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
         <h1 className="text-3xl font-bold">Home</h1>
+
+        <div>
+          <Link href={"hello"}>Hello</Link>
+        </div>
       </div>
     </main>
   )
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,25 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:3000/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Create Next App/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('hello world link', async ({ page }, testInfo) => {
+  await page.goto('http://localhost:3000/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the hello link.
+  await page.getByRole('link', { name: 'Hello' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Hello World' })).toBeVisible();
+
+  // take screenshot: https://playwright.dev/docs/screenshots
+  // attach screenshot: https://www.youtube.com/watch?v=CNiKT-qCj40
+  await testInfo.attach('hello', {
+    body: await page.screenshot(),
+    contentType: 'image/png'
+  });
 });
